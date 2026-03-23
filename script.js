7eb83a7ae068e3c7bfcbdf671d181b650d41fbfc
Morris <underwater_forests@outlook.com>
2026-03-23 17:38:52 -0400

    feat: implement hover effect when mouse enters div, the color of the square changes and stays like leaving a pen trail

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// M 
+
+
+
 // create loop to create 16 divs and append each 
 for (let i = 0; i < 256; i++) {
     const container = document.querySelector('.container');
     const div = document.createElement('div');
-    div.classList.add(`box-${i+1}`)
-    div.textContent = i + 1;
+    let currentBoxClass = `box-${i+1}`; 
+    div.classList.add(currentBoxClass);
+    
+    // hover effect 
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'green';
+    });
+
     container.appendChild(div);
 }
