8dd5fddfc2a2ad4c39dfe732c7a7093479a53d3d
Morris <underwater_forests@outlook.com>
2026-03-23 19:37:43 -0400

    feat: allow to create grid size of 1x1 up to 100x100

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 // M 
 
-
-
-// create loop to create 16 divs and append each 
-for (let i = 0; i < 256; i++) {
-    const container = document.querySelector('.container');
-    const div = document.createElement('div');
-    let currentBoxClass = `box-${i+1}`; 
-    div.classList.add(currentBoxClass);
-    
-    // hover effect 
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = 'green';
-    });
-
-    container.appendChild(div);
+function refreshGrid() {
+    let container = document.querySelector('.container');
+    container.innerHTML = '';
 }
+
+// Grid size selection
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    // cal refresh grid here?
+    refreshGrid();
+
+    input = +prompt('Current grid size is 16x16.\n\nEnter size to create a grid of size 100x100 or less: ');
+    
+    // limit size to 100x100
+    while (input > 100 || input < 1) {
+        input = prompt('Size must be of 1 - 100')
+    }
+
+    // call createGrid function
+    console.log('stub');
+
+    createGrid(input);
+});
+
+function createGrid(size) {
+    let gridSize = size * size;
+
+    // Initial size is 16x16 grid of divs and append each 
+    for (let i = 0; i < gridSize; i++) {
+        const container = document.querySelector('.container');
+        const div = document.createElement('div');
+        let currentBoxClass = `box-${i+1}`; 
+        div.classList.add(currentBoxClass);
+        
+        // hover effect 
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = 'green';
+        });
+
+        // div.textContent = i + 1;
+
+        container.appendChild(div);   
+    }
+}
+
+// Initial grid start - Size 16x16 
+createGrid(16);
